8c469b97ac97332c14d86488aa4f66c0daef1ccc
bt <biantaobrad@gmail.com>
2020-02-25 00:10:03 +0800

    多维数组

--- chap3/delete.js
+++ chap3/delete.js
@@ -2,6 +2,16 @@ let numbers = [0,1,2,3,4,5,6,7,8,9]
 console.log(numbers)
 numbers.pop()
 console.log(numbers)
+numbers.splice(3,2)
+console.log('splice', numbers)
+/**
+ * @param 添加、删除的开始索引
+ * @param 要删除元素的个数
+ * @param 第三个往后，是要添加到数组里的值
+ */
+numbers.splice(3,0,'a','b')
+console.log('splice添加', numbers)
+
 
 /**
  * 删除第一个元素
---
+++ chap3/matrix.js
@@ -0,0 +1,13 @@
+let arr = []
+
+for(let i = 0; i < 3; i++) {
+  arr[i] = []
+  for (let j = 0; j < 3; j++) {
+    arr[i][j] = []
+    for (let k = 0; k < 3; k++) {
+      arr[i][j][k] = i + j + k
+    }
+  }
+}
+
+console.log(arr)
