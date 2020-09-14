5d9fd353f0f65a589cc3096f0e50f2f6dac3e9c6
bt <biantaobrad@gmail.com>
2020-09-14 10:14:08 +0800

    Stack类完成

--- chap4/stack-array.js
+++ chap4/stack-array.js
@@ -7,30 +7,42 @@ class Stack {
    * 添加一个（或几个）新元素到栈顶
    * @param {*} element 
    */
-  push(element) {}
+  push(element) {
+    this.items.push(element)
+  }
 
   /**
    * 移除栈顶的元素，同时返回被移除的元素
    */
-  pop() {}
+  pop() {
+    return this.items.pop()
+  }
 
   /**
    * 返回栈顶的元素，不对栈做任何修改（该方法不会移除栈顶的元素， 仅仅返回它）
    */
-  peek() {}
+  peek() {
+    return this.items[this.items.length -1]
+  }
 
   /**
    * 如果栈里没有任何元素就返回 true，否则返回 false
    */
-  isEmpty() {}
+  isEmpty() {
+    return this.items.length === 0
+  }
 
   /**
    * 移除栈里的所有元素
    */
-  clear() {}
+  clear() {
+    this.items = []
+  }
 
   /**
    * 返回栈里的元素个数。该方法和数组的 length 属性很类似
    */
-  size() {}
+  size() {
+    return this.items.length
+  }
 }
