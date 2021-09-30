155b3ffeee24dfe753818b7b66d86cbc6781ff34
btbrad <24836323+btbrad@users.noreply.github.com>
2021-09-30 23:51:33 +0800

    双向链表

---
+++ chap6/DoublyLinkedList.js
@@ -0,0 +1,77 @@
+import { Node } from '../modules/linked-list-modules'
+import LinkedList from './LinkedList'
+
+class DoublyNode extends Node {
+  constructor(element, next, prev) {
+    super(element, next, prev)
+    this.prev = prev
+  }
+}
+
+class DoublyLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    super(equalsFn)
+    this.tail = undefined
+  }
+
+  // 在任意位置插入新元素
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new DoublyNode(element)
+      let current = this.head
+      if (index === 0) {
+        if (this.head == null) {
+          this.head = node
+          this.tail = node
+        } else {
+          node.next = this.head
+          current.prev = node
+          this.head = node
+        }
+      } else if (index === this.count) {
+        current = this.tail
+        current.next = node
+        node.prev = current
+        this.tail = node
+      } else {
+        const previous = this.getElementAt(index - 1)
+        current = previous.next
+        node.next = current
+        previous.next = node
+        current.prev = node
+        node.prev = previous
+      }
+      this.count++
+      return true
+    }
+    return false
+  }
+
+  // 从任意位置移除元素
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head
+      if (index === 0) {
+        this.head = current.next
+        // 如果链表只有一项，则需要更新tail
+        if (this.count === 1) {
+          this.tail = undefined
+        } else {
+          this.head.prev = undefined
+        }
+      } else if (index === this.count - 1) {
+        current = this.tail
+        this.tail = current.prev
+        this.tail.next = undefined
+      } else {
+        current = this.getElementAt(index)
+        const previous = current.prev
+        previous.next = current.next
+        current.next.prev = previous
+      }
+      this.count--
+      return current.element
+    }
+    return undefined
+  }
+}
