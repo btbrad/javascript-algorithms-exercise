4ed98261074d9cd45f41468110472355b58a8091
btbrad <24836323+btbrad@users.noreply.github.com>
2021-10-20 00:14:41 +0800

    搜索值

--- chap10/BinarySearchTree.js
+++ chap10/BinarySearchTree.js
@@ -93,4 +93,21 @@ export default class BinarySearchTree {
     }
     return current
   }
+
+  // 搜索一个特定的值
+  search(key) {
+    return this.searchNode(this.root, key)
+  }
+  searchNode(node, key) {
+    if (node == null) {
+      return false
+    }
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      return this.searchNode(node.left, key)
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      return this.searchNode(node.right, key)
+    } else {
+      return true
+    }
+  }
 }
