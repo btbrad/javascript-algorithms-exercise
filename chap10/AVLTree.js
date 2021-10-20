3d1e177b304d68b47ad03569c605ff882803357a
btbrad <24836323+btbrad@users.noreply.github.com>
2021-10-20 22:19:57 +0800

    学不动了😞

---
+++ chap10/AVLTree.js
@@ -0,0 +1,20 @@
+import { Compare, defaultCompare } from '../util'
+import BinarySearchTree from './BinarySearchTree'
+
+export default class AVLTree extends BinarySearchTree {
+  constructor(compareFn = defaultCompare) {
+    super(compareFn)
+    this.compareFn = compareFn
+    this.root = null
+  }
+
+  getNodeHeight(node) {
+    if (node == null) {
+      return -1
+    }
+    return (
+      Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) +
+      1
+    )
+  }
+}
