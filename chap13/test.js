44330daaac0be7c989a29543c628fa405b04703f
btbrad <24836323+btbrad@users.noreply.github.com>
2021-10-21 23:07:15 +0800

    选择排序

--- chap13/test.js
+++ chap13/test.js
@@ -1,4 +1,6 @@
 import { bubbleSort } from './bubbleSort'
+import { selectionSort } from './selectionSort'
 
 const array = [5, 4, 3, 2, 1]
 console.log(bubbleSort(array).join(','))
+console.log(selectionSort(array).join(','))
