108d80759b3f23f2faefb2e857fffdd5367507f4
btbrad <24836323+btbrad@users.noreply.github.com>
2021-10-21 21:48:46 +0800

    冒泡排序

---
+++ chap13/test.js
@@ -0,0 +1,4 @@
+import { bubbleSort } from './bubbleSort'
+
+const array = [5, 4, 3, 2, 1]
+console.log(bubbleSort(array).join(','))
--- util.js
+++ util.js
@@ -25,3 +25,10 @@ export function defaultCompare(a, b) {
   }
   return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
 }
+
+export function swap(array, a, b) {
+  // const temp = array[a]
+  // array[a] = array[b]
+  // array[b] = temp
+  ;[array[a], array[b]] = [array[b], array[a]]
+}
